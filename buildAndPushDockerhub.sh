f727c5913eacf4d595fc956d1e9093374f96f880
Henry Chan <henry.chan@opendoor.com>
2023-11-03 10:30:53 -0700

    reads githubapp with vault

--- buildAndPushDockerhub.sh
+++ buildAndPushDockerhub.sh
@@ -1,4 +1,6 @@
 # docker login --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD
 #export TAG=2.0.0
-export TAG=latest
-docker buildx build --platform linux/amd64 -t opendoor/git-resource:$TAG --build-arg base_image=paketobuildpacks/run-jammy-base:latest . --push
+#export TAG=latest
+export TAG=dev
+#docker buildx build --platform linux/amd64 -t opendoor/git-resource:$TAG --build-arg base_image=paketobuildpacks/run-jammy-base:latest . --push
+docker buildx build --platform linux/amd64 -t opendoor/git-resource:$TAG --build-arg base_image=ubuntu:20.04 . --push

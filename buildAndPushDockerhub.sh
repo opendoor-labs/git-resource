c72e48118e04421023508f19bbbfbafff2f4b527
Henry Chan <henry.chan@opendoor.com>
2023-11-03 16:17:55 -0700

    modified to 2.0.2

--- buildAndPushDockerhub.sh
+++ buildAndPushDockerhub.sh
@@ -1,6 +1,5 @@
 # docker login --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD
 #export TAG=2.0.2
-#export TAG=latest
 export TAG=dev
 #docker buildx build --platform linux/amd64 -t opendoor/git-resource:$TAG --build-arg base_image=paketobuildpacks/run-jammy-base:latest . --push
 docker buildx build --platform linux/amd64 -t opendoor/git-resource:$TAG --build-arg base_image=ubuntu:20.04 . --push

b57cf2268c31034f53634e06100af394d38dc832
Jonah Back <jonah.back@opendoor.com>
2025-05-12 08:43:26 -0700

    update base image

--- buildAndPushDockerhub.sh
+++ buildAndPushDockerhub.sh
@@ -2,4 +2,4 @@
 #export TAG=2.0.2
 export TAG=dev
 #docker buildx build --platform linux/amd64 -t opendoor/git-resource:$TAG --build-arg base_image=paketobuildpacks/run-jammy-base:latest . --push
-docker buildx build --platform linux/amd64 -t opendoor/git-resource:$TAG --build-arg base_image=ubuntu:20.04 . --push
+docker buildx build --platform linux/amd64 -t opendoor/git-resource:$TAG --build-arg base_image=ubuntu:24.10 . 
